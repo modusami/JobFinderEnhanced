29b7d2ab21b330b30ab7ed8463533c76927b049a
modusami <128193218+modusami@users.noreply.github.com>
2024-03-29 12:37:16 -0400

    Set default value for extra stats

--- src/components/MainPage/MainPageComponents/Statistic.jsx
+++ src/components/MainPage/MainPageComponents/Statistic.jsx
@@ -4,7 +4,7 @@ const Statistic = ({ statistic_name, statistic_value, extra_stats }) => {
 			<div className="bg-white p-6 rounded-lg shadow-md">
 				<h2 className="text-xl font-bold mb-2">{statistic_name}</h2>
 				<p className="text-4xl font-bold">{statistic_value}</p>
-				<p className="text-gray-500">{extra_stats ? extra_stats : ""}</p>
+				<p className="text-gray-500">{extra_stats ? extra_stats : "Last 7 days: 100.6%"}</p>
 			</div>
 		</>
 	);
