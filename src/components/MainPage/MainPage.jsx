a6b6d50fb2a68bc2e4bc40f35dfd838deb36a7c0
modusami <128193218+modusami@users.noreply.github.com>
2024-03-29 12:50:13 -0400

    Update MainPage to load entries

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,4 +1,33 @@
 import Statistic from "./MainPageComponents/Statistic";
+import Entry from "./MainPageComponents/Entry";
+import backupLogo from "/backup_logo.png";
+
+const sampleData = [
+	{
+		id: "Q4qPJ2M",
+		role: "Staff Software Engineer - Query Optimization",
+		company_name: "mongoDB",
+		location: "Remote USA, NYC, SF, Seattle, etc.",
+		url: "https://findwork.dev/Q4qPJ2M/staff-software-engineer-query-optimization-at-mongodb",
+		logo: "https://findwork-dev-images.s3.amazonaws.com/mongoDB",
+	},
+	{
+		id: "npRpJBM",
+		role: "Multiple engineering roles",
+		company_name: "Constantiam Biosciences",
+		location: "Houston, TX, USA",
+		url: "https://findwork.dev/npRpJBM/multiple-engineering-roles-at-constantiam-biosciences",
+		logo: backupLogo,
+	},
+	{
+		id: "Xxqo94n",
+		role: "Various Engineering, Product, Analytics roles",
+		company_name: "Course Hero",
+		location: "Redwood City, USA; Toronto, Canada; Vancouver, Canada; Remote - India",
+		url: "https://findwork.dev/Xxqo94n/various-engineering-product-analytics-roles-at-course-hero",
+		logo: backupLogo,
+	},
+];
 
 const MainPage = () => {
 	return (
@@ -10,6 +39,12 @@ const MainPage = () => {
 					<Statistic statistic_name={"Count"} statistic_value={"0"} />
 					<Statistic statistic_name={"Most Frequent Skill"} statistic_value={"React"} />
 					<Statistic statistic_name={"Most Frequent Location"} statistic_value={"USA"} />
+				</div>
+				<div className="flex-1 p-8">
+					<h1 className="text-3xl font-bold mb-4">Recent Patients</h1>
+					{sampleData.map((entry) => (
+						<Entry key={entry.id} {...entry} />
+					))}
 				</div>
 			</div>
 		</>
