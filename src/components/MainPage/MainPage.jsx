da7fcf3c5b4af65d0867f340f81e38c36d4c141c
modusami <128193218+modusami@users.noreply.github.com>
2024-03-29 17:43:58 -0400

    Able to load nes headlines

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,9 +2,10 @@ import Statistic from "./MainPageComponents/Statistic";
 import Entry from "./MainPageComponents/Entry";
 import backupLogo from "/backup_logo.png";
 import { useEffect } from "react";
-const token = null;
-const URL_FOR_ALL_DATA = null;
+const API_KEY = import.meta.env.VITE_FINDWORK_API_KEY;
+
 import { useState } from "react";
+import axios from "axios";
 
 const sampleData = [
 	{
@@ -34,20 +35,24 @@ const sampleData = [
 ];
 
 const MainPage = () => {
+	const URL_FOR_ALL_DATA = `https://newsapi.org/v2/top-headlines?country=us&apiKey=bdd28d4b8aba4880a6ba243ebd2cc0d7`;
 	const [mainData, setMainData] = useState(null);
+	const [totalResults, setTotalResults] = useState(0);
 
-	// useEffect(() => {
-	// 	const fetchData = async () => {
-	// 		let headers = new Headers();
-	// 		headers.append("Authorization", "Token " + token);
-	// 		const response = await fetch(URL_FOR_ALL_DATA, {
-	// 			headers: headers,
-	// 		});
-	// 		console.log(response);
-	// 		setMainData(response.results);
-	// 	};
-	// 	fetchData().catch(console.error());
-	// }, []);
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		try {
+			const response = await axios.get(URL_FOR_ALL_DATA);
+			const data = response.data;
+			setTotalResults(data.totalResults);
+			setMainData(data.articles);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
 	return (
 		<>
@@ -55,15 +60,17 @@ const MainPage = () => {
 				<h1 className="text-3xl font-bold mb-4">Main Page</h1>
 				{/* Add your main page content here */}
 				<div className="grid grid-cols-3 gap-4">
-					<Statistic statistic_name={"Count"} statistic_value={"0"} />
+					<Statistic statistic_name={"Total Results"} statistic_value={totalResults} />
 					<Statistic statistic_name={"Most Frequent Skill"} statistic_value={"React"} />
 					<Statistic statistic_name={"Most Frequent Location"} statistic_value={"USA"} />
 				</div>
 				<div className="flex-1 p-8">
-					<h1 className="text-3xl font-bold mb-4">Recent Patients</h1>
-					{sampleData.map((entry) => (
-						<Entry key={entry.id} {...entry} />
-					))}
+					<h1 className="text-3xl font-bold mb-4">News Headlines</h1>
+					{mainData ? (
+						mainData.map((entry, index) => <Entry key={index} {...entry} />)
+					) : (
+						<></>
+					)}
 				</div>
 			</div>
 		</>
--- src/components/MainPage/MainPageComponents/Entry.jsx
+++ src/components/MainPage/MainPageComponents/Entry.jsx
@@ -1,12 +1,17 @@
-const Entry = ({ id, role, company_name, location, url, logo }) => {
+import backupLogo from "/backup_logo.png";
+const Entry = ({ id, title, author, location, url, urlToImage }) => {
 	return (
 		<div className="bg-white p-4 rounded-lg shadow-md mb-4">
 			<a href={url} target="_blank" rel="noopener noreferrer">
 				<div className="flex items-center">
-					{logo && <img src={logo} alt={company_name} className="w-8 h-8 mr-4" />}
+					{urlToImage ? (
+						<img src={urlToImage} alt={author} className="w-8 h-8 mr-4" />
+					) : (
+						<img src={backupLogo} alt={author} className="w-8 h-8 mr-4" />
+					)}
 					<div>
-						<h3 className="text-xl font-bold">{role}</h3>
-						<p className="text-gray-600">{company_name}</p>
+						<h3 className="text-xl font-bold">{title}</h3>
+						<p className="text-gray-600">{author}</p>
 						<p className="text-gray-500">{location}</p>
 					</div>
 				</div>
